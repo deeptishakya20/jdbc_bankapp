e17a9d3a0192021622e277d6ea49399307f65194
Shakya <deepti.shakya@capgemini.com>
2019-03-30 14:50:00 +0530

    updated

--- src/com/capgemini/bank/util/DbUtil.java
+++ src/com/capgemini/bank/util/DbUtil.java
@@ -11,16 +11,17 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 
 public class DbUtil {
-
-	private static String dburl;
-	private static String username;
-	private static String password;
+	
+	private static String driverClassName = "com.mysql.jdbc.Driver";
+	private static String dburl = "jdbc:mysql://localhost:3306/bankappdb";
+	private static String username = "root";
+	private static String password = "root";
  
 	static Connection connection;
     static Logger logger = Logger.getLogger(DbUtil.class);
 	public static Connection getconnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(driverClassName);
 			if(connection == null)
 			connection = DriverManager.getConnection(dburl, username, password);
 			connection.setAutoCommit(false);
@@ -55,7 +56,7 @@ public class DbUtil {
 	
 	
 
-	static {
+	/*static {
 		try {
 			File propertiesFile = new File("dbConfig.properties");
 			FileReader reader = new FileReader(propertiesFile);
@@ -64,12 +65,13 @@ public class DbUtil {
 			properties.load(reader);
 			reader.close();
 
+			driverClassName = properties.getProperty("driverClassName");
 			dburl = properties.getProperty("dburl");
 			username = properties.getProperty("username");
 			password = properties.getProperty("password");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 }
